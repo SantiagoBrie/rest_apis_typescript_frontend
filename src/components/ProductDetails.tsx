1182d55f5ce7b7e1c951ac42141f0f79bca53573
SantiagoBrie <155693553+SantiagoBrie@users.noreply.github.com>
2024-04-15 14:42:35 -0300

    first commit

---
+++ src/components/ProductDetails.tsx
@@ -0,0 +1,77 @@
+import { ActionFunctionArgs, Form, redirect, useFetcher, useNavigate } from "react-router-dom"
+import { Product } from "../types"
+import { formatCurrency } from "../utils"
+import { deleteProduct } from "../services/ProductoService"
+
+type ProductDetailsProps = {
+    product: Product
+}
+
+export async function action({ params }: ActionFunctionArgs) {
+    if (params.id !== undefined) {
+        await deleteProduct(+params.id)
+        return redirect('/')
+    }
+}
+export default function ProductDetails({ product }: ProductDetailsProps) {
+    const fetcher = useFetcher() //// useFetcher se utiliza para realizar acciones sin cambiar de pagina como dale Like a un post en Twitter
+    const navigate = useNavigate()
+    const isAvailable = product.availability
+
+    return (
+        <tr className="border-b ">
+            <td className="p-3 text-lg text-gray-800">
+                {product.name}
+            </td>
+            <td className="p-3 text-lg text-gray-800">
+                {formatCurrency(product.price)}
+            </td>
+            <td className="p-3 text-lg text-gray-800">
+                <fetcher.Form method="POST">
+                    <button
+                        type="submit"
+                        // name="availability"
+                        // value={product.availability.toString()}
+                        name="id"
+                        value={product.id}
+                        className={`${isAvailable ? 'text-black' : 'text-red-600'} rounded-lg p-2 text-xs uppercase font-bold w-full border 
+                                    border-black-100 hover:cursor-pointer`
+                        }
+                    >
+                        {isAvailable ? 'Disponible' : 'No Disponible'}
+                    </button>
+                </fetcher.Form>
+            </td>
+            <td className="p-3 text-lg text-gray-800 ">
+                <div className="flex gap-2 items-center">
+
+                    <button
+                        onClick={() => navigate(`/productos/${product.id}/editar`, {
+                            state: {
+                                product
+                            }
+                        })}
+                        className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase, font-bold text-xs text-center"
+                    >Editar</button>
+
+                    <Form
+                        className="w-full"
+                        method="POST"
+                        action={`productos/${product.id}/eliminar`}
+                        /// onSubmit se ejecuta ANTES que el action
+                        onSubmit={(e) => {
+                            if (!confirm('¿Eliminar?')) {
+                                e.preventDefault()
+                            }
+                        }}
+                    >
+                        <input
+                            type="submit"
+                            value='Eliminar'
+                            className="bg-red-600 text-white rounded-lg w-full p-2 uppercase, font-bold text-xs text-center"
+                        />
+                    </Form>
+                </div>
+            </td>
+        </tr>)
+}
